4040a307f208ea83c13ea34b2f8e2280be105f82
Amonologue <amonologue@AmonologuedeMacBook-Air.local>
2023-06-26 12:48:30 +0800

    Day 2 completed.

--- Day 1-10/Day 2.cpp
+++ Day 1-10/Day 2.cpp
@@ -1,16 +1,3 @@
-# 300 Java code every day by CPP
-This repository is the CPP implementation of [日撸 Java 三百行](https://blog.csdn.net/minfanphd/article/details/116974889)
-## Contents
-
-### Day 1-10
-
-#### Day 1: 在 Visual Studio Code 中配置 CPP 环境
-
-[VSCode配置C/C++环境](https://zhuanlan.zhihu.com/p/87864677)
-
-#### Day 2: basic operation
-
-```c++
 #include <iostream>
 
 int main()
@@ -51,6 +38,3 @@ int main()
     std::cout << tempFirstInt << " % " << tempSecondInt << " = " << tempResultInt << std::endl;
     return 0;
 }
-```
-
-#### Day 3: 
